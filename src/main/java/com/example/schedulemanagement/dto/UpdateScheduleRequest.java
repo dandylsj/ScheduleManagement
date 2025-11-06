f4929005ee3c24a0a0424845a2b71ef9ff1ff7e5
dandylsj <lsjyahoo@gmail.com>
2025-11-07 00:02:43 +0900

    feat: Lv 4 기능 수정
    1.스케줄 수정시 비밀번호가 일치 하지 않을경우 예외처리 추가
    
    2.dto UpdateScheduleRequest 에서 body 값이에 아이디를 요청 받을 필요가 없어서 제거

--- src/main/java/com/example/schedulemanagement/dto/UpdateScheduleRequest.java
+++ src/main/java/com/example/schedulemanagement/dto/UpdateScheduleRequest.java
@@ -6,7 +6,10 @@ import lombok.Getter;
 public class UpdateScheduleRequest {
 
     //속성
+    private Long id; //아이디 값
     private String title; //제목 값
     private String content; //내용 값
     private String password; //패스워드 값
+
+
 }
